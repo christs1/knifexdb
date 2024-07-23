747d2734ea11fba00382cf2e06604c49f49ee211
jaekwon1222 <103784001+jaekwon1222@users.noreply.github.com>
2024-07-23 20:15:18 +0000

    fixed about

--- 362website-main/frontend/src/Pages/About.jsx
+++ 362website-main/frontend/src/Pages/About.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import './CSS/about.css';
+import Image from '../Components/Assets/cs2banner.png'
 
 export const About = () => {
   return (
-    <div>
-
+    <div className="about-container">
+      <div className="about-box">
+        <img src={Image} alt="Knife" className="about-image" />
+        <p>At Knifex, we offer the best CS:GO knife skins <br />to elevate your gaming experience.<br />Discover your perfect blade today! <br /> No refund, All sales are final.</p>
+      </div>
     </div>
   )
 }
